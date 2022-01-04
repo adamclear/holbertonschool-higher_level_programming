7038fc549921cfcce81a99037a7427d9e887dead
Adam Clear <3589@holbertonschool.com>
2022-01-04 23:11:54 +0000

    corrected file 8 to reflect what the assignment was instead of what I assumed it was. My bad!

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-def islower(c):
-    if ord(c) > 64 and ord(c) < 91:
-        return True
-    else:
-        return False
+def uppercase(str):
+    for i in str:
+        if ord(i) > 96 and ord(i) < 123:
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
+    print()
