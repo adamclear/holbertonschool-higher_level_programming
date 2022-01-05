79ddad30dd8087e8693be851ed841ea42b992f13
Adam Clear <3589@holbertonschool.com>
2022-01-05 21:56:29 +0000

    corrected comparison values on file 1 ifs/elifs

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 0:
+    if len(sys.argv) == 1:
         print("0 arguments.")
-    elif len(sys.argv) == 1:
+    elif len(sys.argv) == 2:
         print("1 argument:")
         print("1: {}".format(sys.argv[1]))
     else:
-        x = 0
+        x = 1
         print("{} arguments:".format(len(sys.argv)))
         for i in len(sys.argv):
-            print("{}: {}".format(x + 1, sys.argv[x]))
+            print("{}: {}".format(x, sys.argv[x]))
+            x = x + 1
