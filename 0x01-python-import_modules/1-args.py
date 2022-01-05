d0b3172c4596f9ee22d327eb5105a01aaabab7f7
Adam Clear <3589@holbertonschool.com>
2022-01-05 22:01:31 +0000

    corrected for loop syntax and removed redundant iterator variable

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -7,8 +7,6 @@ if __name__ == "__main__":
         print("1 argument:")
         print("1: {}".format(sys.argv[1]))
     else:
-        x = 1
-        print("{} arguments:".format(len(sys.argv)))
-        for i in len(sys.argv):
-            print("{}: {}".format(x, sys.argv[x]))
-            x = x + 1
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[1]))
