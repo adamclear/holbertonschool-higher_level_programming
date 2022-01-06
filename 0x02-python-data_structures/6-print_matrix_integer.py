cafe8bc92e9eea82717930e9420b2de739fe6b08
Adam Clear <3589@holbertonschool.com>
2022-01-06 21:44:15 +0000

    added counter for proper printing on file 6

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
+        count = 0
         for y in x:
+            if count < len(x):
                 print("{:d}".format(y), end=" ")
+            else:
+                print("{:d}".format(y), end="")
+            count = count + 1
         print()
