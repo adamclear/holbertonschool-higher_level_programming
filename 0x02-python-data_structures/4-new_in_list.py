676bba8d6df11dc806d8ca8660eee0573d532d39
Adam Clear <3589@holbertonschool.com>
2022-01-06 20:54:22 +0000

    changed newlist assignment to my_list.copy() in file 4

--- 0x02-python-data_structures/4-new_in_list.py
+++ 0x02-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    newlist = my_list
+    newlist = my_list.copy()
     if idx < 0 or idx > len(my_list) - 1:
         return newlist
     newlist[idx] = element
