045fd7ee07581a1b1f1d0b9a6a1dcdc03b507a16
Adam Clear <3589@holbertonschool.com>
2022-01-11 21:07:45 +0000

    added a docstring outstide the class init

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+""" This code creates a class named Square. """
 class Square:
-    """Creates a square of a dictated size."""
+    """ Creates a square of a dictated size. """
     def __init__(self, size):
         self.__size = size
