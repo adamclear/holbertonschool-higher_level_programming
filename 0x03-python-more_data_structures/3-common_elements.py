73b84627e602e182fc11f13f242c8fc5a8bebc14
Adam Clear <3589@holbertonschool.com>
2022-01-11 01:14:29 +0000

    changed syntax on file 3 to simple assignment set_1 & set_2

--- 0x03-python-more_data_structures/3-common_elements.py
+++ 0x03-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    com_ele = []
-    set1 = set(set_1)
-    set2 = set(set_2)
-    if (set1 & set2):
-        com_ele.append(set1 & set2)
+    com_ele = set_1 & set_2
     return com_ele
