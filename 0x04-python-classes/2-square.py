27971ccda0bbf3173588d5f3d164fc2f7ee7caae
Adam Clear <3589@holbertonschool.com>
2022-01-11 21:22:32 +0000

    added file 2

---
+++ 0x04-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" This code creates a class named Square. """
+
+
+class Square:
+    """ Creates a square of a dictated size. """
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        area = self.__size * self.__size
+        return area
