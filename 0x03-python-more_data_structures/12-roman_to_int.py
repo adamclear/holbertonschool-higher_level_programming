509d8f2d3408b1ccfa30ecb02d9a66fb5d0aaa1e
Adam Clear <3589@holbertonschool.com>
2022-01-11 05:50:19 +0000

    put all variable assignments on one line in file 12

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    value = 0
-    tempval = 0
-    preval = 0
+    value, tempval, preval = 0, 0, 0
     if not roman_string or not isinstance(roman_string, str):
         return 0
     for x in roman_string:
