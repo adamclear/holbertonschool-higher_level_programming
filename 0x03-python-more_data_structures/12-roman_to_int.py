714aae1ad053436d875e112b25efee7fdd8e26d0
Adam Clear <3589@holbertonschool.com>
2022-01-11 03:21:28 +0000

    corrected loop and arithmetic on file 12

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -3,13 +3,15 @@ def roman_to_int(roman_string):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     value = 0
     tempval = 0
-    if not roman_string:
+    preval = 0
+    if not roman_string or not isinstance(roman_string, str):
         return 0
     for x in roman_string:
         if x in roman.keys():
             tempval = roman[x]
-        if value < tempval:
-            value = tempval - value
-        elif tempval < value:
-            value = tempval + value
+        if preval < tempval:
+            value = value - preval
+            tempval = tempval - preval
+        value += tempval
+        preval = tempval
     return value
