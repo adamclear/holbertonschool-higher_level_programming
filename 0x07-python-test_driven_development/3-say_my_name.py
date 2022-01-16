263e9819e735c9d24ef260a76587e7f9bcc6e2b7
Adam Clear <3589@holbertonschool.com>
2022-01-16 20:42:01 +0000

    added file 3 and file 3 test

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module contains the function say_my_name.
+python3 -c 'print(__import__("my_module").__doc__)'
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    This function prints, "My name is <first_name> <last_name>".
+    If last_name is not provided, only first_name will print.
+    python3 -c 'print(__import__("my_module").my_function.__doc__)'
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
