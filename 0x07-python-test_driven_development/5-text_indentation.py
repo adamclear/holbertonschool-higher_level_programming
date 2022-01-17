1f3e95fd8405a6d5b1c912567795727ae93c03ac
Adam Clear <3589@holbertonschool.com>
2022-01-17 03:38:09 +0000

    corrected if statement in file 5 and added tests to file 5 test

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -9,18 +9,18 @@ def text_indentation(text):
     This function prints the given 'text', but inserts a blank
     line after every '.', '?', and ':'. 'text' must be a string.
     """
-    y = 0
+    y = 1
     buffer = ''
     if type(text) is not str:
         raise TypeError("text must be a string")
     for x in text:
         buffer = buffer + x
-        y += 1
-        if x == '.' or x == '?' or x == ':':
+        if y == len(text):
+            buffer = buffer.strip()
+            print(buffer)
+        elif x == '.' or x == '?' or x == ':':
             buffer = buffer.strip()
             print(buffer)
             print()
             buffer = ""
-        if y == len(text):
-            buffer = buffer.strip()
-            print(buffer)
+        y += 1
