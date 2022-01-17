bbc6cc004b68a85c521dcb4d5a4b9cf35e15bcb3
Adam Clear <3589@holbertonschool.com>
2022-01-17 20:44:58 +0000

    forgot to add : after if statement in perimeter method

--- 0x06-python-more_classes/1-rectangle.py
+++ 0x06-python-more_classes/1-rectangle.py
@@ -69,6 +69,6 @@ class Rectangle:
         """
         Returns the perimeter of the rectangle.
         """
-        if self.__height == 0 or self.__width == 0
+        if self.__height == 0 or self.__width == 0:
             return 0
         return (self.__width * 2) + (self.__height * 2)
