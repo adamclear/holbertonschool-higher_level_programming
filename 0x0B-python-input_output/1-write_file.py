86b299d1b42ff3c26bd53f4e062b94c8e48ba416
Adam Clear <3589@holbertonschool.com>
2022-01-21 21:28:04 +0000

    added file 1 and test for file 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module contains the method: write_file.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    The method writes a string, 'text', to a file, 'filename'.
+    If the file does not exist, it will create the file.
+    If the file is not empty, it will overwrite the contents.
+    """
+    length = len(text)
+    with open(filename, "w") as x:
+        x.write(text)
