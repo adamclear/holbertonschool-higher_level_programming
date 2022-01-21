938c863cc79a9f7ecece8d8bd87ee544b276449f
Adam Clear <3589@holbertonschool.com>
2022-01-21 01:01:27 +0000

    added file 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module contains the method lookup.
+"""
+
+
+def lookup(obj):
+    """
+    This method returns the list of available attributes and
+    methods of an object.
+    """
+    return dir(obj)
