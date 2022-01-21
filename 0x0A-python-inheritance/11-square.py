a8bf79aae6564e73ae35ca2056c7c1214cc1c901
Adam Clear <3589@holbertonschool.com>
2022-01-21 05:51:13 +0000

    added file 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This module contains the class: Square.
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    This class creates an object, square, that is of a particular
+    size, 'size'.
+    """
+    def __init__(self, size):
+        """
+        This method creates the object.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+        This method returns the area of the object.
+        """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """
+        This method provides the string width and height
+        of the object.
+        """
+        return "[Square] <width>/<height>"
