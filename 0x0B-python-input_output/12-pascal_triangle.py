1f790701ed975982680ebd1813599e32145c199b
Adam Clear <3589@holbertonschool.com>
2022-01-22 05:57:46 +0000

    added file 12, unfinished

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+This module contains the method: pascal_triangle.
+"""
+
+
+def pascal_triangle(n):
+    """
+    This method creates a Pascal Triangle of a particular
+    size, 'n'.
+    """
+    if n <= 0:
+        return []
+    x = 1
+    y = 1
+    prev_row = [1]
+    pascal_matrix = [[1]]
+    while x < n:
+        new_row = []
+        while y < len(prev_row) + 1:
+            try:
+                ldigit = prev_row[y - 1]
+            except Exception:
+                ldigit = 0
+            try:
+                rdigit = prev_row[y +  1]
+            except Exception:
+                rdigit = 0
+            digit = ldigit + rdigit
+            if digit == 0:
+                digit = 1
+            new_row.append(digit)
+            y += 1
+        pascal_matrix.append(new_row)
+        x += 1
+        prev_row = new_row
+    return pascal_matrix
