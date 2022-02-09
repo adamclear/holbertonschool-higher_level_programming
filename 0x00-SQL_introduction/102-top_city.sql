57dd8531437bf7f227556c88c2534d24a00e2837
adamclear <3589@holbertonschool.com>
2022-02-09 21:58:45 +0000

    added file 102

---
+++ 0x00-SQL_introduction/102-top_city.sql
@@ -0,0 +1,8 @@
+-- displays the three cities with highest avg temps for July & August
+SELECT city,
+AVG(value) AS avg_temp
+FROM temperatures
+WHERE month = 7 OR month =8
+GROUP BY city
+ORDER BY avg_temp
+DESC TOP 3;
