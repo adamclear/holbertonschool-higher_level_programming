299d1b0c06c11bd043af02aaf0eccb57caa06895
adamclear <3589@holbertonschool.com>
2022-02-10 22:55:20 +0000

    updated file 6

--- 0x01-SQL_more_queries/6-states.sql
+++ 0x01-SQL_more_queries/6-states.sql
@@ -1,8 +1,8 @@
 -- creates DB hbtn_0d_usa and table 'states'
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
-CREATE TABLE IF NOT EXISTS states(
-    id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY(id)
+    PRIMARY KEY (id)
 );
