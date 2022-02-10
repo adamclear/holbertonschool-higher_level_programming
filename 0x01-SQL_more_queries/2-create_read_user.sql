4dbf78e9af6f556d07cfe0d4e411dba4d916a21c
adamclear <3589@holbertonschool.com>
2022-02-10 22:33:34 +0000

    updated file 2

--- 0x01-SQL_more_queries/2-create_read_user.sql
+++ 0x01-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
 -- creates DB hbtn_0d_2 and user_0d_2, grants user_0d_2 select privilege
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
 
-GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
