992c3bd1a32310839ff4f21b8e82697763174f67
adamclear <3589@holbertonschool.com>
2022-02-24 09:46:35 +0000

    updated file 0

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -3,16 +3,19 @@
 This script lists all states from the DB hbtn_0e_0_usa.
 """
 import MySQLdb
-from sys import argv
 
 
 def select_states():
+    from sys import argv
     dbconn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                              passwd=argv[2], database=argv[3])
+    
     cursor = dbconn.cursor()
+
     cursor.execute("SELECT * FROM states")
     for states in cursor.fetchall():
         print(states)
+
     cursor.close()
     dbconn.close()
 
