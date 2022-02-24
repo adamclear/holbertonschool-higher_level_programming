48dfd9fdc667d2fa7814fdabd46255be18532917
adamclear <3589@holbertonschool.com>
2022-02-24 09:57:07 +0000

    updated file 1

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -6,6 +6,10 @@ import MySQLdb
 
 
 def filter_states():
+    """
+    This script lists all states with a name that starts with 'N'
+    from the DB hbtn_0e_0_usa.
+    """
     from sys import argv
     dbconn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                              passwd=argv[2], database=argv[3])
