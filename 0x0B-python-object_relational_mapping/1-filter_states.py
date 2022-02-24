99d7fff99945d68e1bb844103ae92faf898af0e3
adamclear <3589@holbertonschool.com>
2022-02-24 10:04:54 +0000

    updated file 1

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -1,27 +1,22 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """
-This module contains the method: filter_states.
+This script lists all states with a name that starts with 'N'
+from the DB hbtn_0e_0_usa.
 """
 import MySQLdb
 
 
-def filter_states():
-    """
-    This script lists all states with a name that starts with 'N'
-    from the DB hbtn_0e_0_usa.
-    """
+if __name__ == "__main__":
     from sys import argv
     dbconn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                              passwd=argv[2], database=argv[3])
 
     cursor = dbconn.cursor()
 
-    cursor.execute("SELECT * FROM states")
+    cursor.execute("""SELECT * FROM states
+    WHERE name LIKE 'N%'""")
     for row in cursor.fetchall():
         print(row)
 
     cursor.close()
     dbconn.close()
-
-if __name__ == "__main__":
-    filter_states()
