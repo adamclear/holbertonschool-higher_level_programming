da22281d36afe09d55b510743370ea8fd04eb332
adamclear <3589@holbertonschool.com>
2022-02-24 10:42:14 +0000

    updated file 3

--- 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 This script displays all values in the states table of DB
-hbtn_0e_0_usa where name matches the argument given.
+hbtn_0e_0_usa where name matches the argument given, and is
+protected from SQL injection.
 """
 import MySQLdb
 
@@ -13,18 +14,11 @@ if __name__ == "__main__":
 
     cursor = dbconn.cursor()
 
-    x = 0
-    nonochars = ["'", ",", ";", ":"]
-    for char in argv[4]:
-        if char in nonochars:
-            statename = argv[4][:x]
-        x += 1
-
     cursor.execute("""
         SELECT * FROM states
         WHERE name LIKE BINARY '{}'
         ORDER BY states.id ASC
-        """.format(statename))
+        """.format(argv[4].split(" ")[0]))
 
     for row in cursor.fetchall():
         print(row)
