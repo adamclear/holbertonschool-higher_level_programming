dbca39ea000dd21f03c5a83adb2e01a2e1c0cdca
adamclear <3589@holbertonschool.com>
2022-02-24 09:55:56 +0000

    updated file 1

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -2,5 +2,22 @@
 """
 This module contains the method: filter_states.
 """
+import MySQLdb
 
 
+def filter_states():
+    from sys import argv
+    dbconn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                             passwd=argv[2], database=argv[3])
+
+    cursor = dbconn.cursor()
+
+    cursor.execute("SELECT * FROM states")
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    dbconn.close()
+
+if __name__ == "__main__":
+    filter_states()
