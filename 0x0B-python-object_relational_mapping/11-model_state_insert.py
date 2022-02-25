c08fd88e4deb8f62b978eea997c006daac24677e
adamclear <3589@holbertonschool.com>
2022-02-25 01:57:42 +0000

    added file 11

---
+++ 0x0B-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This script lists the State object from the DB hbtn_0e_6_usa
+that matches the name passed as argument, safe from SQL injection.
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    dbengine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                             .format(argv[1], argv[2], argv[3]),
+                             pool_pre_ping=True)
+
+    Base.metadata.create_all(dbengine)
+    dbsession = sessionmaker(bind=dbengine)
+
+    newstate = State(name="Louisiana")
+    dbsession().add(newstate)
+    dbsession().commit()
+    print("{}".format(newstate.id))
+
+    dbsession().close
