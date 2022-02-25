db7be873cddd286abb929655bd6a58d5ae6d0f2f
adamclear <3589@holbertonschool.com>
2022-02-25 03:16:45 +0000

    updated file 14

--- 0x0B-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0B-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -5,7 +5,8 @@ This script lists all City objects from the DB hbtn_0e_14_usa.
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State, City
+from model_state import Base, State
+from model_city import City
 
 
 if __name__ == "__main__":
