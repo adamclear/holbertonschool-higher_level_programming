da100596912b3ac21821377c6aa0c0f1e91290c4
adamclear <3589@holbertonschool.com>
2022-03-09 02:33:16 +0000

    updated file 2

--- 0x0C-python-network_1/2-post_email.py
+++ 0x0C-python-network_1/2-post_email.py
@@ -7,11 +7,11 @@ This module displays the response of a POST request
 if __name__ == '__main__':
     from urllib.request import Request, urlopen
     from urllib.parse import urlencode
-    import sys
+    from sys import argv
 
-    email = {'email': sys.argv[2]}
-    data = urlencode(email).encode('UTF-8')
-    req = Request(sys.argv[1], email)
+    email = {'email': argv[2]}
+    data = urlencode(email).encode('utf-8')
+    request = Request(argv[1], data)
 
-    with urlopen(req) as reply:
-        print(reply.read().decode('UTF-8'))
+    with urlopen(request) as reply:
+        print(reply.read().decode('utf-8'))
