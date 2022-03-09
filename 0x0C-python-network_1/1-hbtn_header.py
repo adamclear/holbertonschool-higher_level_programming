d1f3fec7926864037c48f390af4f7183b0d04982
adamclear <3589@holbertonschool.com>
2022-03-09 02:05:21 +0000

    added file 1

---
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module displayes the value of the X-Request-Id variable
+"""
+
+
+if __name__ == '__main__':
+    import urllib.request
+    import urllib.parse
+    import sys
+
+    with urllib.request.urlopen(sys.argv[1]) as reply:
+        html = reply.info()
+        print('{}'.format(html.get('X-Request-ID')))
