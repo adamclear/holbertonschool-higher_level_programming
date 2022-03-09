070d273a408a861e598f736f5ccb67840e20e300
adamclear <3589@holbertonschool.com>
2022-03-09 03:56:34 +0000

    added file 10

---
+++ 0x0C-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Displays GitHub id using the GitHub API
+"""
+
+
+if __name__ == '__main__':
+    from requests import get
+    from requests.auth import HTTPBasicAuth
+    from sys import argv
+    requests = get('https://api.github.com/user',
+                   auth=HTTPBasicAuth(argv[1], argv[2])).json()
+    print(requests.get('id'))
