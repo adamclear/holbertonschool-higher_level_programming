948aff2792606abaa9a0859acfbc4e1e64ed1ff5
adamclear <3589@holbertonschool.com>
2022-04-07 19:39:22 +0000

    added file 1

---
+++ 0x02-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const fileserver = require('fs');
+fileserver.writeFile(process.argv[2], process.argv[3],
+  'utf-8', function (error, content) {
+    if (error) {
+      console.error(error);
+    }
+  });
